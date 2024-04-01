39848c5a8481daeaa77270bbf636c27491ec523f
Bibek <Bibek@DESKTOP-8JQ78AV>
2024-03-31 20:49:12 -0400

    Add project files.

---
+++ wpftryyyyyyyyyyyy/CourseRatingsPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace AcademicAssistanceProject
+{
+    public partial class CourseRatingsPage : UserControl
+    {
+        private List<CourseRating> _courseRatings;
+
+        public CourseRatingsPage()
+        {
+            InitializeComponent();
+            LoadCourseRatings();
+        }
+
+        private void LoadCourseRatings()
+        {
+            // Example data
+            _courseRatings = new List<CourseRating>
+            {
+                  
+            
+                new CourseRating("High Quality Programming ", 4.5),
+                new CourseRating("Software Testing tools", 4.8),
+                new CourseRating("Software Analysis and Design", 4.7),
+                new CourseRating("Software Testing Methodologies", 4.7),
+                new CourseRating("Software Engineering Principles", 4.7)
+            };
+
+            CourseListBox.ItemsSource = _courseRatings;
+        }
+    }
+}
---
+++ wpftryyyyyyyyyyyy/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace AcademicAssistanceProject
+{
+    public class CourseRating
+    {
+        public string CourseName { get; set; }
+        public double Rating { get; set; }
+
+        public CourseRating(string courseName, double rating)
+        {
+            CourseName = courseName;
+            Rating = rating;
+        }
+    }
+}
